caa9053f713a61827ce1c2431b6d860a8fa270a0
Wendroff <624101034@qq.com>
2017-12-07 14:47:17 +0800

    sys_test
    try to read parameters for command line

---
+++ Sys_argv/sys_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  7 14:36:43 2017
+
+@author: wendroff
+"""
+
+import sys
+print(len(sys.argv))
+print('script names: '+sys.argv[0])
+
+print(sys.argv[1])
